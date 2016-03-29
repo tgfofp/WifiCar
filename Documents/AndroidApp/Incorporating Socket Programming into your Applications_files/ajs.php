3a00c6f497f4fc4f45c0309e5c6c754475006c4d
tgfofp <tgfofp@gmail.com>
2016-03-29 21:26:56 +0300

    Research web pages

---
+++ Documents/AndroidApp/Incorporating Socket Programming into your Applications_files/ajs.php
@@ -0,0 +1,9 @@
+(function(){
+var s = "<"+"script type=\'text/javascript\' src=\'//www.googletagservices.com/tag/js/gpt.js\'>\n";
+s += "  googletag.pubads().definePassback(\'/9240792/criteo_mrec_passback\', [300, 250]).setTargeting(\'SSL\', [(\'https:\' == document.location.protocol ? \'1\' : \'0\')]).display();\n";
+s += "<"+"/script>\n";
+s += "<"+"div id=\'beacon_e98cb05d06\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"https://cat.fr.eu.criteo.com/delivery/lg.php?cppv=1&cpp=jCHKanw4S0RiMXhJUjBiTVNrdFNoUit4MkVWYXBDZ3YrZjRxT3YzQnoybENlNVk3N1Z0MGFyMjdrZG5HK1krMDFnN0dlMzNaaXJYSXY1bUFMeC9yU2FmdktzdWRXMDZvTFc4b1psMWxjS2w3UHpmMDJaUFVKaW5iRmhScmJVa2VDNlhsZ3ZhUmdvNm85L3VCb1JpdnZaSGhXU1htQjFhcEZmY3hPSFJjNktuWFV6T2dZKzB2elgwOXRWQUdna3ZZcm1ISytoZDlSRlVHeUNBNDA4UmxGbUdLdWplbElJZGJrTFRwWnJxOXZkMUlFbnRRPXw%3D\"/>\n";
+s += "<"+"/div>\n";
+s += "\n";
+document.write(s);})();
