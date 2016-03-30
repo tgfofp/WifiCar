74bff8e7d7e5af1ff0a84a094454dc62d60ac023
tgfofp <tgfofp@gmail.com>
2016-03-30 16:32:12 +0300

    Client can connect to Server by IP and port

--- app/src/main/java/com/example/gpaskov/wificarapp/MainActivity.java
+++ app/src/main/java/com/example/gpaskov/wificarapp/MainActivity.java
@@ -5,11 +5,30 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText serverIp;
+    private EditText serverPort;
+    private Button button;
+
+    private String serverIpAddress = "";
+    private int serverPortNumber;
+
+    private boolean connected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +37,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        serverIp = (EditText) findViewById(R.id.server_ip);
+        serverPort = (EditText) findViewById(R.id.port_number);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(connectListener);
+
+    }
+
+    private OnClickListener connectListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if (!connected) {
+                serverIpAddress = serverIp.getText().toString();
+                serverPortNumber = Integer.valueOf(serverPort.getText().toString());
+                if (!serverIpAddress.equals("") && serverPortNumber > 0) {
+                    Thread cThread = new Thread(new ClientThread());
+                    cThread.start();
+                }
             }
-        });
+        }
+    };
+
+    public class ClientThread implements Runnable {
+
+        public void run() {
+            try {
+                InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
+                Log.d("ClientActivity", "C: Connecting...");
+                Socket socket = new Socket(serverAddr, serverPortNumber);
+                connected = true;
+                while (connected) {
+                    try {
+                        Log.d("ClientActivity", "C: Sending command.");
+                        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                                .getOutputStream())), true);
+                        // WHERE YOU ISSUE THE COMMANDS
+                        out.println("Hey Server!");
+                        Log.d("ClientActivity", "C: Sent.");
+                    } catch (Exception e) {
+                        Log.e("ClientActivity", "S: Error", e);
+                    }
+                }
+                socket.close();
+                Log.d("ClientActivity", "C: Closed.");
+            } catch (Exception e) {
+                Log.e("ClientActivity", "C: Error", e);
+                connected = false;
+            }
+        }
     }
 
     @Override
