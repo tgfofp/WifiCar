a616d1d4d9547ed7f4b6e7020c22ee064b820a2c
tgfofp <tgfofp@gmail.com>
2016-04-04 01:38:19 +0300

    Added general knowledge documents together with client .apk

---
+++ app/src/main/java/com/example/gpaskov/wificarapp/DisplayMessageActivity.java
@@ -0,0 +1,35 @@
+package com.example.gpaskov.wificarapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+//        TextView textView = new TextView(this);
+//        textView.setTextSize(40);
+//        textView.setText(message);
+//
+//        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+//        layout.addView(textView);
+    }
+
+}
--- app/src/main/java/com/example/gpaskov/wificarapp/MainActivity.java
+++ app/src/main/java/com/example/gpaskov/wificarapp/MainActivity.java
@@ -1,113 +1,49 @@
 package com.example.gpaskov.wificarapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.app.Activity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private EditText serverIp;
-    private EditText serverPort;
-    private Button button;
-
-    private String serverIpAddress = "";
-    private int serverPortNumber;
-
-    private boolean connected = false;
+    TextView response;
+    EditText editTextAddress, editTextPort;
+    Button buttonConnect, buttonClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        serverIp = (EditText) findViewById(R.id.server_ip);
-        serverPort = (EditText) findViewById(R.id.port_number);
-        button = (Button) findViewById(R.id.button);
+        editTextAddress = (EditText) findViewById(R.id.addressEditText);
+        editTextPort = (EditText) findViewById(R.id.portEditText);
+        buttonConnect = (Button) findViewById(R.id.connectButton);
+        buttonClear = (Button) findViewById(R.id.clearButton);
+        response = (TextView) findViewById(R.id.responseTextView);
 
-        button.setOnClickListener(connectListener);
+        buttonConnect.setOnClickListener(new OnClickListener() {
 
-    }
-
-    private OnClickListener connectListener = new OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            if (!connected) {
-                serverIpAddress = serverIp.getText().toString();
-                serverPortNumber = Integer.valueOf(serverPort.getText().toString());
-                if (!serverIpAddress.equals("") && serverPortNumber > 0) {
-                    Thread cThread = new Thread(new ClientThread());
-                    cThread.start();
-                }
+            @Override
+            public void onClick(View arg0) {
+                Client myClient = new Client(editTextAddress.getText()
+                        .toString().trim(), Integer.parseInt(editTextPort
+                        .getText().toString().trim()), response);
+                myClient.execute();
             }
-        }
-    };
+        });
 
-    public class ClientThread implements Runnable {
+        buttonClear.setOnClickListener(new OnClickListener() {
 
-        public void run() {
-            try {
-                InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
-                Log.d("ClientActivity", "C: Connecting...");
-                Socket socket = new Socket(serverAddr, serverPortNumber);
-                connected = true;
-                while (connected) {
-                    try {
-                        Log.d("ClientActivity", "C: Sending command.");
-                        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
-                                .getOutputStream())), true);
-                        // WHERE YOU ISSUE THE COMMANDS
-                        out.println("Hey Server!");
-                        Log.d("ClientActivity", "C: Sent.");
-                    } catch (Exception e) {
-                        Log.e("ClientActivity", "S: Error", e);
-                    }
-                }
-                socket.close();
-                Log.d("ClientActivity", "C: Closed.");
-            } catch (Exception e) {
-                Log.e("ClientActivity", "C: Error", e);
-                connected = false;
+            @Override
+            public void onClick(View v) {
+                response.setText("");
             }
-        }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
