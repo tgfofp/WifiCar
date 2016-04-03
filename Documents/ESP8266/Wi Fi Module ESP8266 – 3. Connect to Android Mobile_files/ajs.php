35be393f30601d37e899bdb90b1cfbf95b5412f3
tgfofp <tgfofp@gmail.com>
2016-04-04 01:58:54 +0300

    Removing unnecessary documents

--- Documents/ESP8266/Wi Fi Module ESP8266 – 3. Connect to Android Mobile_files/ajs.php
+++
@@ -1,9 +0,0 @@
-(function(){
-var s = "<"+"script type=\'text/javascript\' src=\'//www.googletagservices.com/tag/js/gpt.js\'>\n";
-s += "  googletag.pubads().definePassback(\'/9240792/criteo_mrec_passback\', [300, 250]).setTargeting(\'SSL\', [(\'https:\' == document.location.protocol ? \'1\' : \'0\')]).display();\n";
-s += "<"+"/script>\n";
-s += "<"+"div id=\'beacon_4921c2a414\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
-s += "<"+"img width=\"0\" height=\"0\" src=\"https://cat.fr.eu.criteo.com/delivery/lg.php?cppv=1&cpp=uYGtmXxvR0hsdVNQMnJmVTlyT09VaGhwNnk0YTdOaGt0LzVWRkxFU1kralh4cE0vTDE5clFCaThoRFFSeW8xK3FPeVJDNVN0eTBGdWoyWFF3OFg2YjhvQndhODVTN21qUTljS05TYlVZTEluSkYwQ3djbEpaMGNML0tBbU9MblRmVGQ2VThBNkNoRExpb2twdWExaVhacUZhNUJiZXBJckJYaEdScHk5aks2UWNVbWRIbDNLUGt3QkpGTk5maGk1eFgxS1hMZnZpTVcrNUdXdjJzTDJPeEp5M01QaHhDZmRNOURBRWRWYWdHcVh4Q2o4PXw%3D\"/>\n";
-s += "<"+"/div>\n";
-s += "\n";
-document.write(s);})();
--- Documents/ESP8266/WiFi Module ESP8266 – 1. Getting started with AT commands_files/ajs.php
+++
@@ -1,9 +0,0 @@
-(function(){
-var s = "<"+"script type=\'text/javascript\' src=\'//www.googletagservices.com/tag/js/gpt.js\'>\n";
-s += "  googletag.pubads().definePassback(\'/9240792/criteo_mrec_passback\', [300, 250]).setTargeting(\'SSL\', [(\'https:\' == document.location.protocol ? \'1\' : \'0\')]).display();\n";
-s += "<"+"/script>\n";
-s += "<"+"div id=\'beacon_bab00b6cf6\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
-s += "<"+"img width=\"0\" height=\"0\" src=\"https://cat.fr.eu.criteo.com/delivery/lg.php?cppv=1&cpp=y04k6nx5V0J5RFd2SG01V3VNNllUZkMwZWJFaVJyYS9kQTZtRzAxSWNBRE8rVU1mVHFnL2hnSnFXSjkybFRoQ3ZoOXg5TUdOM2lPRnl5YTY4RGpsZmR6UmdNMVRwUEwwZmM0TDd6M3dja3dGdDZvMGgreElNenlXRFoxODQwRHUweVNlY1BxVUhweVJNMy96cXF0akEwc1UwK2QxWTBZd3d5cFlRd0lPYVV2SytWSHVGNzl6N2xWTGZIdkhvRFB1am1UQWQwdWtONmVQWkI3ckpMc3hLNzBGaXAyODYvZWpObjd4aVNTNkZubWxXUVgwPXw%3D\"/>\n";
-s += "<"+"/div>\n";
-s += "\n";
-document.write(s);})();
